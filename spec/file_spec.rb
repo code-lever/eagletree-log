15ce29a255eab9df5b43b111d265523377860a0a
Nick Veys <nickveys@gmail.com>
2013-05-31 12:13:15 -0500

    Adding more file specs

--- spec/file_spec.rb
+++ spec/file_spec.rb
@@ -2,11 +2,69 @@ require 'spec_helper'
 
 describe EagleTree::Log::File do
 
+  context 'data file empty.fdr' do
+
+    subject { EagleTree::Log::File.new(data_file('empty.fdr')) }
+
+    it { should have(0).sessions }
+
+    its(:name) { should eql('Skywalker') }
+
+    its(:hardware) { should eql('74') }
+
+    its(:version) { should eql(10.04) }
+
+  end
+
+  context 'data file funjet-gps.fdr' do
+
+    subject { EagleTree::Log::File.new(data_file('funjet-gps.fdr')) }
+
+    it { should have(1).sessions }
+
+    its(:name) { should eql('FunJet') }
+
+    its(:hardware) { should eql('73') }
+
+    its(:version) { should eql(8.03) }
+
+  end
+
+  context 'data file multi-session-1.fdr' do
+
+    subject { EagleTree::Log::File.new(data_file('multi-session-1.fdr')) }
+
+    it { should have(3).sessions }
+
+    its(:name) { should eql('Helis') }
+
+    its(:hardware) { should eql('73') }
+
+    its(:version) { should eql(6.93) }
+
+  end
+
+  #context 'data file multi-session-2.fdr' do
+  #
+  #  subject { EagleTree::Log::File.new(data_file('multi-session-2.fdr')) }
+  #
+  #  it { should have(3).sessions }
+  #
+  #  its(:name) { should eql('Losi XXX4') }
+  #
+  #  its(:hardware) { should eql('71') }
+  #
+  #  its(:version) { should eql(6.02) }
+  #
+  #end
+
   context 'data file t600-1.fdr' do
 
     subject { EagleTree::Log::File.new(data_file('t600-1.fdr')) }
 
     it { should have(1).sessions }
+
+    its(:name) { should eql('T-Rex 600') }
 
     its(:hardware) { should eql('73') }
 
@@ -19,6 +77,8 @@ describe EagleTree::Log::File do
     subject { EagleTree::Log::File.new(data_file('t600-2.fdr')) }
 
     it { should have(1).sessions }
+
+    its(:name) { should eql('T-Rex 600') }
 
     its(:hardware) { should eql('73') }
 
